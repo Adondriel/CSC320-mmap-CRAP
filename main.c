306d6f5490893df4179c2410f4d6b620f7f00cdf
Adam Pine <adondrielcraft@gmail.com>
2017-05-03 13:59:57 -0400

    wip

--- main.c
+++ main.c
@@ -3,12 +3,17 @@
  * Courtney Rush
  * Adam Pine
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <memory.h>
 
+#define _LARGEFILE64_SOURCE
+#include <stdlib.h>
+#include <memory.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define _FILE_OFFSET_BITS 64
 #define bufsize 500
 #define delim " "
 
@@ -38,8 +43,38 @@ char **tokenizeLine(char *line) {
     return tokens;
 }
 
-int main(int argc, char *argv[]) {
+char **tokenizeAddr(char *line) {
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token, **tokens_backup;
+    int pos = 0;
+    int newbuf = 0;
+    token = strtok(line, "-");
+    /* walk through other tokens */
+    while (token != NULL) {
+        newbuf += bufsize;
+        tokens[pos] = token;
+        pos++;
+        if (pos >= bufsize) {
+            tokens_backup = tokens;
+            tokens = realloc(tokens, newbuf * sizeof(char *));
+            if (!tokens) {
+                free(tokens_backup);
+                fprintf(stderr, "allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        //printf("%s ", token);
+        token = strtok(NULL, "-");
+    }
+    return tokens;
+}
 
+uint64_t getPageNumber(uint64_t addr) {
+    int pagesize = getpagesize();
+    return addr / pagesize;
+}
+
+int main(int argc, char *argv[]) {
     if (argc == 2) {
         //One command line argument
         int pid = atoi(argv[1]);
@@ -64,18 +99,33 @@ int main(int argc, char *argv[]) {
                 //assemble array of lines.
                 while (fgets(line, sizeof(line), fp)) {
                     tokens = tokenizeLine(line);
-                    if (atoi(tokens[4]) > 0) {
-                        char newSegment[256];
-                        strcpy(newSegment, tokens[5]);
-                        if (strcmp(newSegment, lastSegment)) {
-                            printf("\n----------------------------------------------------\n");
-                            strcpy(lastSegment, newSegment);
-                        }
-                        for(int i=0; i <= 5; i++){
-                            printf("%s ", tokens[i]);
-                        }
-                        //printf("\nnth index: %s\n", tokens[5]);
+                    char newSegment[256];
+                    strcpy(newSegment, tokens[5]);
+                    char **addrTokens = tokenizeAddr(tokens[0]);
+                    //printf("%s %s\n", addrTokens[0], addrTokens[1]);
+                    if (strcmp(newSegment, lastSegment)) {
+                        printf("Segment: %s", newSegment);
+                        printf("%s", tokens[1]);
+                        printf("\n----------------------------------------------------\n");
+                        strcpy(lastSegment, newSegment);
                     }
+                    unsigned char *buf = (unsigned char *) malloc(16);
+                    uint64_t startAddrL = strtol(addrTokens[0], NULL, 16);
+                    uint64_t endAddrL = strtol(addrTokens[1], NULL, 16);
+                    FILE *pmFP = fopen(pagemapStr, "rb");
+                    lseek64(fileno(pmFP), 8 * getPageNumber(startAddrL), SEEK_SET);
+                    fread(buf, 1, 16, pmFP);
+                    for(int i=0; i < 16; i++){
+                        printf("%2x", buf[i]);
+                    }
+                    printf("\n");
+                    //char *buf;
+                    //buf = read(pmFP, buf, getPageNumber(_FILE_OFFSET_BITS));
+                    //printf("%s\n", buf);
+                    //for(int i=0; i <= 5; i++){
+                    //    printf("%s", tokens[i]);
+                    //}
+                    //printf("\nnth index: %s\n", tokens[5]);
 
 
                 }
